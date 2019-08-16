132c2043c94e664ae91aa97fa9625907ec720c0e
Steven H. Wang <wang.steven.h@gmail.com>
2019-08-16 13:05:38 -0700

    fix install

--- chai_install.sh
+++ chai_install.sh
@@ -4,7 +4,6 @@ set -e
 #
 # Assumes that relevant apt-get packages from
 # scripts/linux_setup.sh are already installed.
-conda env create create -f environment.yml
+conda env create -f environment.yml
 conda activate rllab
 pip install -e .
-yes | pip install mujoco_py
