7c1b37f9d979e7b1ba9bec9fc48843903f3de136
Steven H. Wang <wang.steven.h@gmail.com>
2019-08-16 11:52:22 -0700

    An evokative name

--- chai_install.sh
+++ chai_install.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
+set -e
 # Installs rllab on CHAI server
+#
 # Assumes that relevant apt-get packages from
 # scripts/linux_setup.sh are already installed.
 conda env create create -f environment.yml
