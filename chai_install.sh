45ce7e94f82b4ec765cfe508f84c81e898e4acda
Steven H. Wang <wang.steven.h@gmail.com>
2019-08-16 13:14:39 -0700

    Update install

--- chai_install.sh
+++ chai_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
-# Installs rllab on CHAI server
+# Installs rllab locally on CHAI server in conda environment `rllab3`.
 #
 # Assumes that relevant apt-get packages from
 # scripts/linux_setup.sh are already installed.
 conda env create -f environment.yml
-conda activate rllab
+conda activate rllab3
 pip install -e .
